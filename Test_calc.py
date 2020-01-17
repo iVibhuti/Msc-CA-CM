2877cfd34f98bce6fac1eddf2f066c1e9487f7e9
adisamak28 <59257603+adisamak28@users.noreply.github.com>
2020-01-18 00:41:09 +0530

    Add files via upload

---
+++ Test_calc.py
@@ -0,0 +1,14 @@
+import singmat
+import unittest
+import calc
+class Testcalc(unittest.TestCase):
+    def test_sample(self):
+        #result=calc.sample([[1,4],[2,8]])
+        self.assertEqual(calc.sample([[1,4],[2,8]]),0.0)
+        self.assertEqual(calc.sample([[1,3],[2,5]]),-1.0)
+        self.assertEqual(calc.sample([[1,2],[2,4]]),0.0)
+        self.assertEqual(calc.sample([[1,1],[2,3]]),1.0) 
+if __name__=='__main__':
+    unittest.main()
+    
+        
