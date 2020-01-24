d33b8a6ea598e8bde30976bb2085268b8389090d
edmartis <59271700+edmartis@users.noreply.github.com>
2020-01-25 01:48:00 +0530

    Add files via upload

---
+++ Assignment1 Submission/testMat.py
@@ -0,0 +1,19 @@
+"""Name:Edna Martis
+PRN:19030142015"""
+import unittest
+from SingMat import matrix
+
+
+class TestMatrix(unittest.TestCase):
+
+    def test_square_matrix(self):
+        self.assertTrue(matrix([[5, 1], [2, 3]]))
+
+    def test_determinant(self):
+        self.assertEqual(matrix([[0, 1], [0, 1]]), 0)
+        self.assertNotEqual(matrix([[0, 1], [1, 1]]), 0)
+        self.assertEqual(matrix([[1, 2], [3, 5]]), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
