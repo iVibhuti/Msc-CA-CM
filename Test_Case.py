a51439390677837a85891259bdaf0955b5f8ad84
saluja777 <51714455+saluja777@users.noreply.github.com>
2020-01-25 15:04:53 +0530

    CM Assignment
    Prn :19030142004
    Aman Saluja 
    Msc. Ca

---
+++ Test_Case.py
@@ -0,0 +1,22 @@
+#Name- Aman Saluja
+#PRN - 19030142004
+
+
+import unittest
+from SingMat import matrix
+
+
+
+class TestMatrix(unittest.TestCase):
+
+    def test_square_matrix(self):
+        self.assertTrue(matrix([[5, 1], [2, 3]]))
+
+    def test_determinant(self):
+        self.assertEqual(matrix([[0, 1], [0, 1]]), 0)
+        self.assertNotEqual(matrix([[0, 1], [1, 1]]), 0)
+        self.assertEqual(matrix([[1, 2], [3, 5]]), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
