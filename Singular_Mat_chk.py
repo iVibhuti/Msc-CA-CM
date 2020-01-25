447d446172e7f6150aa6b703c9531cdef394582e
Ishan Patel <34618540+ishanpatel27@users.noreply.github.com>
2020-01-25 22:16:34 +0530

    CM assignment
    Author : ISHAN PATEL 
    PRN : 19030142013

---
+++ Singular_Mat_chk.py
@@ -0,0 +1,33 @@
+#Name : ISHAN PATEL
+#PRN - 19030142013
+
+def matrix(l):
+    d = 0
+    if len(l) == 1:
+        return l[0][0]
+    if len(l) == 2:
+        m = l[0][0] * l[1][1] - l[0][1] * l[1][0]
+        return m
+
+    for i in range(len(l)):
+        if len(l) > 1:
+            cf = l[1:]
+            cf = list(map(lambda x: x[:i] + x[i + 1:], cf))
+            d += (-1) ** i * l[0][i] * matrix(cf)
+        else:
+            return d
+    else:
+        return d
+
+
+if __name__ == '__main__':
+    f = open('inputfile', 'r')
+    l = [[int(num) for num in line.split(',')] for line in f]
+    print(l)
+    matrix(l)
+    if matrix(l) == 0:
+        print("Determinant of the Matrix:", matrix(l))
+        print("Singular")
+    else:
+        print("Determinant of the Matrix:", matrix(l))
+        print("Not Singular")
