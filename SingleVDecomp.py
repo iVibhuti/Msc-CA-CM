44dc8a4c950267dd6c93a467bc5c226965fb9ade
beinganki04 <ankitaacharekar04@gmail.com>
2020-03-08 17:59:13 +0530

    Update SingleVDecomp.py

--- SingleVDecomp.py
+++ SingleVDecomp.py
@@ -1,8 +1,8 @@
 import numpy as np
-a = np.array([[1, 0, 0, 0, 2], [0, 0, 3, 0, 0], [0, 0, 0, 0, 0], [0, 2, 0, 0, 0]], dtype=np.float32)
+a = np.array([[1, 0, 0, 0, 2], [0, 0, 3, 0, 1], [0, 0, 2, 0, 0], [0, 2, 0, 0, 0]], dtype=np.float32)
 print("Given array:")
 print(a)
-U, s, V = np.linalg.svd(a, full_matrices=False)
+x, y, z = np.linalg.svd(a, full_matrices=False)
 q, r = np.linalg.qr(a)
 print("Factor of a given array by Singular Value Decomposition:")
-print("U=\n", U, "\ns=\n", s, "\nV=\n", V)
+print("x=\n", x, "\ny=\n", y, "\nz=\n", z)
