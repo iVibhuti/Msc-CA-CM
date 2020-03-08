76f4d321999aa0c9e3dc320e02dd19df7ca66628
shriaas2898 <aasthashrivastava410@gmail.com>
2020-03-09 01:13:13 +0530

    Added documentation in svd file

--- assignment2/svd.py
+++ assignment2/svd.py
@@ -1,4 +1,7 @@
 import matrix.mat_operations as matop
+'''author: shriaas2898
+   description: Python file contains function to derive 3 matrices from a single matrix by the method of singular value 
+   decomposition '''
 # Singular value decompostion of 2x2 matrix
 
 
@@ -6,13 +9,18 @@ def svd(matrix):
     if len(matrix) != 2:
         WrongOrderMatrix = Exception("Matrix: ", matrix, " is not of 2x2 order")
         raise WrongOrderMatrix
+    # extracting elements from matrix
     m1, m2 = matrix[0]
     m3, m4 = matrix[1]
 
-    e1 = ((m1 + m2) + ((m1 - m4) ** 2 + 4 * m2 * m3) ** 0.5) / 2
-    e2 = ((m1 + m2) - ((m1 - m4) ** 2 + 4 * m2 * m3) ** 0.5) / 2
+    # finding roots of quadratic equation: e**2 -(m1+m2)*e -m2*m3
+    e1 = ((m1 + m4) + ((m1 - m4) ** 2 + 4 * m2 * m3) ** 0.5) / 2
+    e2 = ((m1 + m4) - ((m1 - m4) ** 2 + 4 * m2 * m3) ** 0.5) / 2
 
+    # building eigen matrix from eigen values
     evalue_matrix = [[e1, 0], [0, e2]]
+
+    # calculating eigen vectors
     evector_matrix = []
     e = e1
     x = m2
